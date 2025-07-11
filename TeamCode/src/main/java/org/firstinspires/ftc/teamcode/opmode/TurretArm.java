7d646e2b233aeb7a0ddb342dd0624a0f43711a7e
thriftypiano <rickzhou314@gmail.com>
2025-07-11 13:57:03 -0700

    fixed IK function. robot can now move to any 3D coordinates.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/TurretArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/TurretArm.java
@@ -86,7 +86,7 @@ public class TurretArm extends LinearOpMode {
     public double[] startPosArray = servoInitialPosArray.clone();
     public double[] endPosArray = servoInitialPosArray.clone();
 
-    public double[] xyzPos = new double[]{0, 40, 15};
+    public double[] xyzPos = new double[]{0, 40, 0};
 
     @Override
     public void runOpMode() {
@@ -140,25 +140,25 @@ public class TurretArm extends LinearOpMode {
             double zDelta = 0;
 
             if (gamepad1.dpad_up) {
-                yDelta += 0.002;
+                yDelta += 0.005;
             }
             if (gamepad1.dpad_down) {
-                yDelta -= 0.002;
+                yDelta -= 0.005;
             }
             if (gamepad1.dpad_left) {
-                xDelta -= 0.002;
+                xDelta -= 0.005;
             }
             if (gamepad1.dpad_right) {
-                xDelta += 0.002;
+                xDelta += 0.005;
             }
 
             float stickY = gamepad1.left_stick_y;
             // Apply deadzone to left_stick_y
             if (Math.abs(stickY) > 0.1) {
                 if (stickY > 0) { // Stick pushed down (positive Y value)
-                    zDelta += 0.002;
+                    zDelta -= 0.005;
                 } else { // Stick pushed up (negative Y value, stickY < -0.1)
-                    zDelta -= 0.002;
+                    zDelta += 0.005;
                 }
             }
 
@@ -166,8 +166,6 @@ public class TurretArm extends LinearOpMode {
                 xyzPos[0] += xDelta;
                 xyzPos[1] += yDelta;
                 xyzPos[2] += zDelta;
-                setServoAnglesDegrees(calculateAngles(xyzPos), 0.2);
-                xyzUpdatedByDpadOrStick = true;
             }
 
             // Only process A, B, X buttons if DPad/Stick didn't command a move in this cycle
@@ -175,7 +173,13 @@ public class TurretArm extends LinearOpMode {
                 if (gamepad1.a) {
                     setServoAnglesDegrees(new double[]{0, 0, 0, 0, 0, 0}, 1);
                 } else if (gamepad1.b) {
-                    setServoAnglesDegrees(new double[]{30, 30, 30, 30, 30, 0}, 1);
+                    double[] calculatedAngles = calculateAngles(xyzPos);
+                    setServoAnglesDegrees(calculatedAngles, 1);
+                    // Add telemetry for calculated angles
+                    for (int i = 0; i < calculatedAngles.length; i++) {
+                        telemetry.addData("Calculated (" + SERVO_NAMES[i] + ")", "%.4f", calculatedAngles[i]);
+                    }
+                    xyzUpdatedByDpadOrStick = true;
                 } else if (gamepad1.x) {
                     setServoAnglesDegrees(new double[]{0, 5.768, 45.842, -49.926, 0, -10}, 1);
                 }
@@ -229,25 +233,27 @@ public class TurretArm extends LinearOpMode {
 
         // Turret Angle (degrees)
         // Calculated from atan2, converted to degrees, and adjusted by -90 degrees.
-        armAngles_deg[0] = 90 - Math.toDegrees(Math.atan2(relativePosition[1], relativePosition[0]));
+        armAngles_deg[0] = Math.toDegrees(Math.atan2(relativePosition[1], relativePosition[0])) - 90;
 
         // Arm1 Angle (degrees, absolute, relative to horizontal)
         // Intermediate arm1Angle_rad is kept in radians for use in arm2Angle calculation.
         double val_for_acos = (d * d + h * h + q * q - r * r) / (2 * q * Math.sqrt(d * d + h * h));
         val_for_acos = Math.max(-1.0, Math.min(1.0, val_for_acos)); // Clamp to avoid NaN from acos
-        double arm1Angle_rad = Math.atan2(h, d) - Math.acos(val_for_acos);
+        double arm1Angle_rad = - Math.atan2(h, d) + Math.acos(val_for_acos);
         armAngles_deg[1] = Math.toDegrees(arm1Angle_rad);
 
         // Arm2 Angle (degrees, absolute, relative to horizontal)
         // Calculated using arm1Angle_rad (in radians) for sin/cos, then result converted to degrees.
         // This formula determines the absolute angle arm2 needs to make with the horizontal
         // to point from its pivot (end of arm1) to the target.
-        double arm2Angle_rad = Math.atan2(h - q * Math.sin(arm1Angle_rad), d - q * Math.cos(arm1Angle_rad));
-        armAngles_deg[2] = Math.toDegrees(arm2Angle_rad + arm1Angle_rad);
+        double arm2Angle_rad = Math.atan2(h + q * Math.sin(arm1Angle_rad), d - q * Math.cos(arm1Angle_rad)) + arm1Angle_rad;
+        if (arm2Angle_rad < 0) {
+            arm2Angle_rad += Math.PI;
+        }
+        armAngles_deg[2] = Math.toDegrees(arm2Angle_rad);
 
         // Wrist1 Angle (degrees)
-        // Assuming the original intent was (arm1_abs_deg + arm2_abs_deg) - 90 deg.
-        armAngles_deg[3] = 90 - Math.toDegrees(arm2Angle_rad);
+        armAngles_deg[3] = - 90 + (Math.toDegrees(arm2Angle_rad) - Math.toDegrees(arm1Angle_rad));
 
         // Wrist2 Angle (degrees) - Placeholder, not calculated from XYZ
         armAngles_deg[4] = 0.0;
